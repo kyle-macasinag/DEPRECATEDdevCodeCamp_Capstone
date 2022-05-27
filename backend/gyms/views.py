5dfdf321f2cdfc730b8a55c9f4b81e69399c0aea
kyle.macasinag <kylenmac@verizon.net>
2022-05-27 12:53:01 -0400

    CHORE: Modified the app urls & views files + modified project urls file + returning a 200 status code

--- backend/gyms/views.py
+++ backend/gyms/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(["GET"])
+def gyms_list(request):
+    return Response("ok")
