bb805cbcb74e4e0def1cefd496e78b1e5dc609e9
kyle.macasinag <kylenmac@verizon.net>
2022-05-27 14:17:02 -0400

    CHORE: Modified gyms.views.  Now returns an empty list instead of 'ok'

--- backend/gyms/views.py
+++ backend/gyms/views.py
@@ -1,6 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import GymSerializer
+from .models import Gym
 
 @api_view(["GET"])
 def gyms_list(request):
-    return Response("ok")
+    gyms = Gym.objects.all()
+    serializer = GymSerializer(gyms, many=True)
+    return Response(serializer.data)
+    
