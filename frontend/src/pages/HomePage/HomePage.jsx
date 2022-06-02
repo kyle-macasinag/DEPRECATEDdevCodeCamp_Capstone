c14070b77cdc0d7063ff0aba685529e7e92fb44c
kyle.macasinag <kylenmac@verizon.net>
2022-06-02 17:13:43 -0400

    CHORE: Home page has a newly integrated map

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,24 @@
 import { Component } from "react"
+import {Map, GoogleApiWrapper} from "google-maps-react"
 
-export default class MapContainer extends Component {
+class MapContainer extends Component {
     render() {
         return(
-            <div>
-            <h1>Integration Time!</h1>
-            </div>
+            <Map
+            google = {this .props.google}
+            style = {{width:"100%", height:"100%"}}
+            zoom = {10}
+            initalCenter = {
+                {
+                    lat: 27.88079,
+                    lng: -82.32976
+                }
+            }
+            />
         );
     }
 }
+
+export default GoogleApiWrapper({
+    apiKey:"AIzaSyAYovgZ5lEWHFrnuWqM4fBvBD0QEZhH_Pw"
+})(MapContainer)
