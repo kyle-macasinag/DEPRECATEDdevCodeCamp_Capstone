538ca21ad47c7b4a34b2d1e598ae70653982906a
kyle.macasinag <kylenmac@verizon.net>
2022-06-02 16:17:37 -0400

    FIX: App.js once again has a route to the home page.  Home page now displays information

---
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react"
+
+export default class MapContainer extends Component {
+    render() {
+        return(
+            <div>
+            <h1>Integration Time!</h1>
+            </div>
+        );
+    }
+}
