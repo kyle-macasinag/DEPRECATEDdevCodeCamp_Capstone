ee9fd94589dde45ed02b87b864d612897a3f7f46
kyle.macasinag <kylenmac@verizon.net>
2022-06-03 16:50:50 -0400

    FIX: Deleted 3rd party package which conflicted with current version of React (I think). Map generates with no errors

--- frontend/src/pages/MapPage/MapPage.jsx
+++ frontend/src/pages/MapPage/MapPage.jsx
@@ -1,22 +1,9 @@
-import { useState, useMemo } from "react";
+import { useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";
-import "@reach/combobox/styles.css";
 
-export default function Gyms() {
+export default function Home() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyAYovgZ5lEWHFrnuWqM4fBvBD0QEZhH_Pw",
-    libraries: ["places"],
   });
 
   if (!isLoaded) return <div>Loading...</div>;
@@ -25,60 +12,10 @@ export default function Gyms() {
 
 function Map() {
   const center = useMemo(() => ({ lat: 27.88074, lng: -82.32977 }), []);
-  const [selected, setSelected] = useState(null);
 
   return (
-    <>
-      <div className="places-container">
-        <PlacesAutocomplete setSelected={setSelected} />
-      </div>
-
-      <GoogleMap
-        zoom={10}
-        center={center}
-        mapContainerClassName="map-container"
-      >
-        {selected && <Marker position={selected} />}
-      </GoogleMap>
-    </>
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+      <Marker position={center} />
+    </GoogleMap>
   );
 }
-
-const PlacesAutocomplete = ({ setSelected }) => {
-  const {
-    ready,
-    value,
-    setValue,
-    suggestions: { status, data },
-    clearSuggestions,
-  } = usePlacesAutocomplete();
-
-  const handleSelect = async (address) => {
-    setValue(address, false);
-    clearSuggestions();
-
-    const results = await getGeocode({ address });
-    const { lat, lng } = await getLatLng(results[0]);
-    setSelected({ lat, lng });
-  };
-
-  return (
-    <Combobox onSelect={handleSelect}>
-      <ComboboxInput
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-        disabled={!ready}
-        className="combobox-input"
-        placeholder="Search for a gym"
-      />
-      <ComboboxPopover>
-        <ComboboxList>
-          {status === "OK" &&
-            data.map(({ place_id, description }) => (
-              <ComboboxOption key={place_id} value={description} />
-            ))}
-        </ComboboxList>
-      </ComboboxPopover>
-    </Combobox>
-  );
-};
